626cbddbb333827d1de83a3ac0afa6e2cb2386b2
Musa Sakızcı <cyber.blacklight@gmail.com>
2017-05-14 23:13:29 +0300

    Update wordlist.py

--- wordlist.py
+++ wordlist.py
@@ -3,9 +3,105 @@
 
 
 
-words = ['evet',
-             'hayir',
-             'Seviyorum',
-             'Sevmiyorum',
-             'nefret ediyorum',
-             'cok seviyorum']
+words = [	
+'bir',
+
+'bu',
+
+'ne',
+
+'ve',
+
+'mi',
+
+'için',
+
+'çok',
+
+'ben',
+
+'o',
+
+
+'de',
+
+'evet',
+
+'var',
+
+'ama',
+
+'mı',
+
+'değil',
+'da',
+
+'hayır ',
+
+'sen',
+
+'şey',
+
+'daha',
+
+'bana',
+
+'kadar',
+
+'seni',
+
+'beni',
+
+'iyi',
+
+'tamam',
+
+'onu ',
+
+'bunu ',
+
+'gibi',
+
+'yok',
+
+'benim',
+
+'her',
+
+'sana',
+
+'ki',
+
+'sadece',
+
+'neden',
+
+'burada',
+
+'senin',
+
+'ya',
+
+'zaman',
+
+'hiç',
+
+'şimdi',
+
+'nasıl',
+
+'sonra',
+
+'olduğunu',
+
+'en',
+
+'mu ',
+
+'misin',
+
+'hadi',
+
+'şu ',
+]
+
